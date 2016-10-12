152d52227c09a35ba78d6123b900b0872cedd270
Júlio Pilares <juliopilares@gmail.com>
2016-10-11 22:33:34 -0300

    primeiro commit

---
+++ FundamentosDotNet/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundamentosDotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
